fbaf633b942102e32b8cbb4119726f8b3048d0f0
Nikola Metulev <nikolame@microsoft.com>
2021-02-12 10:11:03 -0800

    Moved to user control

---
+++ App34/TextItem.cs
@@ -0,0 +1,9 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App34
+{
+    public class TextItem
+    {
+        public string Text { get; set; }
+    }
+}
